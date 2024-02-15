67655ad41201006e839ad91626641d7632a32d72
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-15 19:13:06 +0100

    start new app

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # with app.app_context():
+    #     db.create_all()
+
+    app.run(debug=True)
