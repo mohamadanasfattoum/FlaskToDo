5554065306ffa543ce58dbc8fecceec479db2a1c
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-16 22:38:42 +0100

    add models name and notes and def init in class Todo

--- models.py
+++ models.py
@@ -6,3 +6,10 @@ db = SQLAlchemy()
 
 class Todo(db.model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100) , nullable=False) 
+    notes = db.Column(db.String(500)) 
+
+    def __init__(self, name, notes=''): # tp show the same name
+        self.name=name
+        self.notes=notes
+    
