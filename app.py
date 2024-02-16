38232c0436d0eb103cdf5ef86179865004c02a50
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-16 23:27:20 +0100

    add db.create_all to app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from models import db, Todo
 
 
 
 app = Flask(__name__)
 
+# flask config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db.init_app(app)
 
-
+# create tables when start as migrate
+with app.app_context():
+    db.create_all()
 
 # views
 @app.route('/') # url
