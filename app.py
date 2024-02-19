4e5c626f7de45f4794d2f922b413d20fd11b0e01
Mohamad anas fattoum <samaanas30@gmail.com>
2024-02-19 19:18:37 +0100

    add base_dir and db_path to add database file in src

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from models import db, Todo
-
+import os
 
 
 app = Flask(__name__)
 
+
+# db path
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(base_dir,'todo.db')
+
 # flask config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
